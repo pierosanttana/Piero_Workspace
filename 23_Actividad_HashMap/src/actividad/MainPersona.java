86a254b65546e85ae301d3f0f8e216e14628bc94
pierosanttana <piero.santtana@gmail.com>
2024-04-23 09:08:19 +0200

    add cambios Main Persona

--- 23_Actividad_HashMap/src/actividad/MainPersona.java
+++ 23_Actividad_HashMap/src/actividad/MainPersona.java
@@ -9,7 +9,7 @@ public class MainPersona {
 	static HashMap<String, Persona> HashMapPersonas;
 
 	public static void main(String[] args) {
-		
+
 		menu();
 
 	}
@@ -24,7 +24,7 @@ public class MainPersona {
 
 		do {
 			System.out.println(" ------------------------------");
-			System.out.println("|[1] Introducir Persona        |");
+			System.out.println("|[1] Ingresar Persona          |");
 			System.out.println("|[2] Mostrar Personas          |");
 			System.out.println("|[3] Buscar Persona por nombre |");
 			System.out.println("|[0] Salir del programa        |");
@@ -55,15 +55,16 @@ public class MainPersona {
 	}
 
 	private static void buscarPersona() {
-		
+
 		System.out.println("-- Buscador --");
 		String nombre = pedirNombre().toLowerCase();
-		if(HashMapPersonas.get(nombre) == null) {
+
+		if (HashMapPersonas.get(nombre) == null) {
 			System.out.println("No se encuentra esa persona.");
-		}else {
-			System.out.println(HashMapPersonas.get(nombre));			
+		} else {
+			System.out.println(HashMapPersonas.get(nombre));
 		}
-		
+
 	}
 
 	private static void mostrarPersonas() {
@@ -76,56 +77,57 @@ public class MainPersona {
 
 	private static void AltaPersona() {
 		Persona p1 = new Persona();
-		
+
 		String nombre = pedirNombre();
 		String nombreBuscado = nombre.toLowerCase();
+
 		String opcion = "";
-		
-		if(HashMapPersonas.get(nombreBuscado) != null) {
-		System.out.println("Esta Persona ya existe, quieres sobreescribirla?");
-		System.out.println("Pulsa 'y' para sobreescribir o 'n' para salir");
-		opcion = scanner.nextLine().toLowerCase();
-		
-		
-		switch(opcion) {
-		case  "y":
-			p1.setNombre(pedirNombre());
+
+		if (HashMapPersonas.get(nombreBuscado) != null) {
+			System.out.println("Esta Persona ya existe, ¿Quieres sobreescribirla?");
+			System.out.println("Pulsa 'y' para sobreescribir o 'n' para salir.");
+			opcion = scanner.nextLine();
+
+			switch (opcion) {
+			case "y":
+			case "Y":
+				p1.setNombre(nombre);
+				p1.setEdad(pedirEdad());
+				p1.setPeso(pedirPeso());
+
+				HashMapPersonas.put(p1.getNombre().toLowerCase(), p1);
+				break;
+			case "n":
+			case "N":
+				System.out.println("");
+				break;
+			default:
+				System.out.println("Opción incorrecta");
+				break;
+			}
+		} else {
+			p1.setNombre(nombre);
 			p1.setEdad(pedirEdad());
 			p1.setPeso(pedirPeso());
-			
-			HashMapPersonas.put(p1.getNombre().toLowerCase(),p1);
-		break;
-		case "n":
-			System.out.println("");
-		break;
-		default:
-		System.out.println("Opción incorrecta");
-		break;
-		}
-		}else {
-		p1.setNombre(nombre);
-		p1.setEdad(pedirEdad());
-		p1.setPeso(pedirPeso());
-		
-		HashMapPersonas.put(p1.getNombre().toLowerCase(),p1);
-		}
-		
-	}
 
+			HashMapPersonas.put(p1.getNombre().toLowerCase(), p1);
+		}
+
+	}
 
 	private static String pedirNombre() {
 		System.out.println("Ingresa un el nombre:");
 		String nombre = scanner.nextLine();
 		return nombre;
 	}
-	
+
 	private static int pedirEdad() {
 		System.out.println("Ingresa la edad:");
 		String sEdad = scanner.nextLine();
 		int iEdad = Integer.parseInt(sEdad);
 		return iEdad;
 	}
-	
+
 	private static double pedirPeso() {
 		System.out.println("Ingresa el peso:");
 		String sPeso = scanner.nextLine();
